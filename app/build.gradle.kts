24689d63d6d85cffbbe6b92c897f55043223ad07
prokopenko.a <kap.moral22@gmail.com>
2025-01-17 15:14:23 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,112 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.kotlin.kapt)
+    id("kotlin-parcelize") // Add this line
+
+}
+
+android {
+    namespace = "com.komus.storage"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.komus.storage"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.storage)
+    implementation(libs.androidx.core.i18n)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation ("com.symbol:emdk:9.1.1")
+    implementation ("com.google.android.gms:play-services-code-scanner:16.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0") {
+        because("kotlin-stdlib-jdk7 is now a part of kotlin-stdlib")
+    }
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0") {
+        because("kotlin-stdlib-jdk8 is now a part of kotlin-stdlib")
+    }
+
+    // Hilt
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.compiler)
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.0")
+    implementation("androidx.compose.material:material-icons-extended:1.1.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+
+    // Fragment KTX для viewModels()
+    implementation ("androidx.fragment:fragment-ktx:1.6.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0") // Для обработки JSON
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0") // Для логирования запросов
+
+    // Compose
+    implementation ("androidx.compose.material3:material3:1.2.0-alpha03")
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation ("androidx.compose.ui:ui:1.5.1")
+    implementation ("androidx.compose.ui:ui-tooling:1.5.1")
+
+    // AppCompat (для обратной совместимости)
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.9.0")
+}
---
+++ app/src/main/java/com/komus/storage/viewModel/ScannerViewModel.kt
@@ -0,0 +1,33 @@
+package com.komus.storage.viewModel
+
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class ScannerViewModel @Inject constructor() : ViewModel() {
+
+    private val _barcodeData = MutableStateFlow("")
+    val barcodeData: StateFlow<String> get() = _barcodeData
+
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> get() = _error
+
+    fun onBarcodeScanned(data: String) {
+        Log.d("ScannerViewModel", "Updating barcode data: $data")
+        _barcodeData.value = data // Установка нового значения
+    }
+    fun clearBarcode() {
+        _barcodeData.value = "" // Сбрасываем данные после обработки
+        _error.value = "" // Сбрасываем ошибки
+    }
+
+    fun onScanError(errorMessage: String) {
+        Log.d("ScannerViewModel", "Updating error: $errorMessage")
+        _error.value = errorMessage
+    }
+}
